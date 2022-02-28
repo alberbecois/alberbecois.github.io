5abe527790009dd0e8efed26006f6ad9866e7701
Johan Elder <94501730+johanelder@users.noreply.github.com>
2022-02-28 17:06:07 -0500

    first pass at JS
    tried validation on the form. no luck right now.

--- scripts/specials.js
+++ scripts/specials.js
@@ -17,12 +17,38 @@ var provinces = ["ON", "QC", "SK", "AB", "BC", "NWT", "YK", "NB", "NS", "PEI", "
 
 
 /// validate name form
-function validateFirstNameForm() {
-    let x = document.forms["input-group-text"]["fName"].value;
-    if (x == "") {
-      alert("Name must be filled out");
-      return false;
-    }
-}
+/**
+ *  validates user data entry
+ */
+ function validateForm() {
+    // declare variables
+    let txtFirstName = document.getElementById('fName');
+    let txtLastName = document.getElementById('lName');
+    let emailInput = document.getElementById('txtEmail');
 
-document.addEventListener('click', validateFirstNameForm, false)
+    // validate poduct
+    if (txtFirstName.value === '') {
+        window.alert('You must enter a first name');
+        txtFirstName.focus();
+        return false;
+    } // end if
+    else
+    // validate quantity
+    if (txtLastName.value === '') {
+        window.alert('You must enter a last name');
+        txtLastName.focus();
+        return false;   
+    } // end if
+    else
+    // validate shipping method
+    if (emailInput.value === '') {
+        window.alert('You must enter a valid email address');
+        emailInput.focus();
+        return false;
+    } // end if
+    else {
+        return true;
+    }
+
+    
+}
