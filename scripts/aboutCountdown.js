0dabac0155eaa3ea800d45cebe6301cd3cfe7de2
midgale <99677443+midgale@users.noreply.github.com>
2022-03-03 14:10:49 -0500

    fixing clock on about pages

--- scripts/aboutCountdown.js
+++ scripts/aboutCountdown.js
@@ -1,12 +1,5 @@
-// JavaScript Document
 
-/* 1. eventClock() - start up function - displays countdown in days, hours, 
-    minutes, seconds to target date
-   2. calculateDays - returns the number of days left between the current
-    date and target date
-*/
-
-
+//function to display two clocks
 function eventClock() {
     
     // create a variable that contains the curret date and time
@@ -17,31 +10,35 @@ function eventClock() {
 
     // display the days left in the txtDaysLeft textbox
     document.getElementById('txtDaysLeft').value = Math.floor(days);
+    document.getElementById('daysLeft').value = Math.floor(days);
 
     // calculate the hours left in the current day
     let hours = ( days -  Math.floor(days)) * 24;
 
     // display hours rounded to the next lowest integer
     document.getElementById('txtHrLeft').value = Math.floor(hours);
+    document.getElementById('hrLeft').value = Math.floor(hours);
 
     // calculate the minutes left to the cuurent hour
     let minutes = (hours - Math.floor(hours)) * 60 ; 
 
     // display minutes rounded to the next lowest integer
     document.getElementById('txtMinLeft').value = Math.floor(minutes);
+    document.getElementById('minLeft').value = Math.floor(minutes);
 
     // calculate the seconds left to the current minute
     let seconds = (minutes - Math.floor(minutes)) * 60;
 
     // display seconds rounded to the next lowest integer
     document.getElementById('txtSecLeft').value = Math.floor(seconds); 
+    document.getElementById('secLeft').value = Math.floor(seconds); 
 }
 
 /**
  * returns the number of days left between the current date and target date
  * @param {Date} currentDate 
  */
-function calculateDays(currentDate) {
+ function calculateDays(currentDate) {
 
     //set the openingDate
     let openingDate = new Date('May 7, 2022 11:30:00');
